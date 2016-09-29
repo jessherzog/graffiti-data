cc16816b665260ccdb079a3e260c1f6549aedfee
Jess <jsscherzog@yahoo.com>
2016-09-29 14:04:46 -0400

    display map

--- leaflet/sketch.js
+++ leaflet/sketch.js
@@ -1,5 +1,9 @@
 // initialize the map and set its view to our chosen geographical coordinates and a zoom level:
-var mymap = L.map('mapid', 'mapbox.streets').setView([40.7850, ‎-73.9682], 13);
+// var mymap = L.map('mapid', 'mapbox.streets').setView([40.7850, ‎-73.9682], 15);
+
+// var mymap = L.map('mapid', 'mapbox.streets').setView([40.7850, ‎-73.9682], 13);
+var mymap = L.map('mapid').setView([40.730610, -73.935242], 13);
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoieGl1eGl1eHh4IiwiYSI6ImNpdDFuM3VxYjBxem4ycHFwcHAzMG5lZHIifQ.dvASaKLVsXfV_2tN2aATMg', {
     attribution: '<a href="http://openstreetmap.org">OpenStreetMap</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, <a href="http://mapbox.com">Mapbox</a>',
@@ -8,11 +12,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
     accessToken: 'pk.eyJ1IjoieGl1eGl1eHh4IiwiYSI6ImNpdDFuM3VxYjBxem4ycHFwcHAzMG5lZHIifQ.dvASaKLVsXfV_2tN2aATMg'
 }).addTo(mymap);
 
-// var runDataLayer = omnivore.gpx('/assets/routes GPX/sept_3.gpx')
-//     .on('ready', function() {
-//         mymap.fitBounds(runDataLayer.getBounds());
-//     })
-//     .addTo(mymap);
+// // var runDataLayer = omnivore.gpx('/assets/routes GPX/sept_3.gpx')
+// //     .on('ready', function() {
+// //         mymap.fitBounds(runDataLayer.getBounds());
+// //     })
+// //     .addTo(mymap);
    
-   console.log(graffitData);
-// 40.7850, ‎-73.9682
+console.log(graffitData);
