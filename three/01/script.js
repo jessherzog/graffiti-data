8c8ae8ed4d1d9e49c01f56ebd93e92916e86734b
mbrav <mbrav@protonmail.com>
2016-10-01 23:13:16 -0400

    Added camera controls, code clean up

--- three/01/script.js
+++ three/01/script.js
@@ -1,75 +1,87 @@
-var viewAngle = 75;
-var aspectRatio = window.innerWidth / window.innerHeight;
-var near = 0.1;
-var far = 400000;
-var camera = new THREE.PerspectiveCamera(viewAngle, aspectRatio, near, far);
-var scene = new THREE.Scene();
-var renderer = new THREE.WebGLRenderer();
-var cube;
-var group;
-var sphere;
-var pointLight = new THREE.PointLight(0xFFFFFF);
-var mouseX = 0;
-var mouseY = 0;
+var camera, scene,renderer;
+var cube,group;
+var pointLight;
 
-// data
-var scale = 150;
+var scale = 150; //   scale/n
 
+// data statistics
 var maxXcordinate = 0;
 var minXcordinate = 999999999;
 var maxYcordinate = 0;
 var minYcordinate = 999999999;
+var middleX;
+var middleY;
 
-var rangeX;
-var rangeY;
+// fly over control
+var controls;
+var clock = new THREE.Clock();
 
-for (var i = 0; i < graffitData.length; i++) {
-  if (graffitData[i][1] != null && graffitData[i][2] != null) {
-    if (maxXcordinate < parseInt(graffitData[i][1])) {
-      maxXcordinate = parseInt(graffitData[i][1]);
-    }
-    if (minXcordinate > parseInt(graffitData[i][1])) {
-      minXcordinate = parseInt(graffitData[i][1]);
-    }
-    if (maxYcordinate < parseInt(graffitData[i][2])) {
-      maxYcordinate = parseInt(graffitData[i][2]);
-    }
-    if (minYcordinate > parseInt(graffitData[i][2])) {
-      minYcordinate = parseInt(graffitData[i][2]);
-    }
-  }
-}
-
+init();
+animatedRender();
 
 function init() {
+    var viewAngle = 75;
+    var aspectRatio = window.innerWidth / window.innerHeight;
+    var near = 0.1;
+    var far = 400000;
+    camera = new THREE.PerspectiveCamera(viewAngle, aspectRatio, near, far);
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer();
 
-    document.addEventListener('mousemove', onDocumentMouseMove, false);
-    window.addEventListener('resize', onWindowResize, false);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
 
-    rangeX = maxXcordinate - minXcordinate;
-    rangeY = maxYcordinate - minYcordinate;
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+    container.appendChild(renderer.domElement);
+    window.addEventListener('resize', onWindowResize, false);
+
+    // calculate statistics
+    for (var i = 0; i < graffitData.length; i++) {
+      if (maxXcordinate < parseInt(graffitData[i][1])) {
+        maxXcordinate = parseInt(graffitData[i][1]);
+      }
+      if (minXcordinate > parseInt(graffitData[i][1])) {
+        minXcordinate = parseInt(graffitData[i][1]);
+      }
+      if (maxYcordinate < parseInt(graffitData[i][2])) {
+        maxYcordinate = parseInt(graffitData[i][2]);
+      }
+      if (minYcordinate > parseInt(graffitData[i][2])) {
+        minYcordinate = parseInt(graffitData[i][2]);
+      }
+    }
+
+    middleX = maxXcordinate - minXcordinate;
+    middleY = maxYcordinate - minYcordinate;
 
     console.log("minXcordinate", minXcordinate, "maxXcordinate", maxXcordinate);
     console.log("minYcordinate", minYcordinate, "maxYcordinate", maxYcordinate);
 
-    createCube();
+    createCubes();
 
+    pointLight = new THREE.PointLight(0xFFFFFF);
     pointLight.position.z = 10;
     camera.position.z = 200;
+
+    controls = new THREE.FlyControls( camera );
+    controls.movementSpeed = 40;
+    controls.domElement = container;
+    controls.rollSpeed = 0.3;
+    controls.autoForward = false;
+    controls.dragToLook = false;
+
+    scene.fog = new THREE.Fog(0xffffff, near, 600);
 
     scene.add(group);
     scene.add(pointLight);
     scene.add(camera);
 
-    console.log(rangeX, rangeY);
-
     // console.log(graffitData);
     // console.log(graffitData[600])0
 }
 
-function createCube() {
+function createCubes() {
 
     var geometry = new THREE.BoxGeometry(1, 1, 1);
     for (var i = 0; i < geometry.faces.length; i += 2) {
@@ -86,26 +98,24 @@ function createCube() {
 
     for (var i = 0; i < graffitData.length; i++) {
 
-      if (graffitData[i][1] != null && graffitData[i][2] != null) {
-        cube = new THREE.Mesh(geometry, material);
-        cube.position.x = (parseInt(graffitData[i][1]) - minXcordinate)/scale - rangeX/scale/2;
-        cube.position.y = (parseInt(graffitData[i][2]) - minYcordinate)/scale - rangeY/scale/2;
-        cube.position.z = 0;
+      cube = new THREE.Mesh(geometry, material);
+      cube.position.x = ((parseInt(graffitData[i][1]) - minXcordinate) - middleX/2)/scale;
+      cube.position.y = ((parseInt(graffitData[i][2]) - minYcordinate) - middleY/2)/scale;
+      cube.position.z = 0;
 
-        cube.updateMatrix();
-        group.add(cube);
-      }
+      cube.updateMatrix();
+      group.add(cube);
 
     }
 }
 
 function animatedRender() {
     requestAnimationFrame(animatedRender);
+    var delta = clock.getDelta();
+    controls.update(delta);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
-    camera.rotation.y -= mouseX * 0.00005;
-    camera.rotation.x -= mouseY * 0.00005;
 
     var d = new Date();
     var n = d.getMilliseconds();
@@ -115,35 +125,8 @@ function animatedRender() {
     renderer.setClearColor(skyColor, 1.0);
 }
 
-function onDocumentMouseMove(event) {
-    mouseX = (event.clientX - window.innerWidth / 2);
-    mouseY = (event.clientY - window.innerHeight / 2);
-}
-
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-
-document.onkeypress = function(evt) {
-    evt = evt || window.event;
-    var charCode = evt.keyCode || evt.which;
-    var charStr = String.fromCharCode(charCode);
-
-    if (charStr == "w" || charStr == "W") {
-        camera.position.z -= 2.0;
-    }
-    if (charStr == "s" || charStr == "S") {
-        camera.position.z += 2.0;
-    }
-    if (charStr == "a" || charStr == "A") {
-        camera.position.x -= 2.0;
-    }
-    if (charStr == "d" || charStr == "D") {
-        camera.position.x += 2.0;
-    }
-};
-
-init();
-animatedRender();
